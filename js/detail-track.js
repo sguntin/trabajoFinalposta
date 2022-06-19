a9d07d5749ab7b68497c758d3cea38b8ef72318c
LorenzoCozzo <lcozzo@udesa.edu.ar>
2022-06-19 03:55:00 -0300

    track

--- js/detail-track.js
+++ js/detail-track.js
@@ -1,26 +1,37 @@
-let urltrack = https://developers.deezer.com/api/track 
+let queryString = location.search;
+let qsToObject = new URLSearchParams(queryString);
+let idCancion = qsToObject.get('id');
+console.log(idCancion);
 
-fetch(urltrack)
-.then*=(function(response){
+let urlTrack = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556'
+
+fetch(urlTrack)
+.then(function(response){
     return response.json();
 })
-.then(function.data){
+.then(function(data){
     console.log(data);
-}
+
+    let titulo = document.querySelector(".nombrecancion");
+    let cantante = document.querySelector(".artistadecancion")
+    let album = document.querySelector(".albumdecancion")
+    let urlDeImagen = data.album.cover
+
+    titulo.innerHTML= data.title 
+    cantante.innerHTML = data.artist.name 
+   
+    
+
+
+    album.innerHTML = data.album.title
+    
+
+    let foto = document.createElement('img')
+    foto.src  = urlDeImagen
+    document.querySelector('.infocancion').appendChild(foto)
+})
 
 .catch(function(error){
     console.log('el error fue ' + error);
 })
-
-
-let foto = document.querySelector(".img");
-
-foto.src = 
-
-let titulo = document.querySelector(".nombrecancion");
-
-titulo.innerHTML += 
-
-let artista = document.querySelector(".artistadecancion");
-artista.innerHTML += 
 
