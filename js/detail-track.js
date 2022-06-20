2026724e970919a1c9622fea12b1e419370668dd
LorenzoCozzo <lcozzo@udesa.edu.ar>
2022-06-19 22:35:18 -0300

    playlist

--- js/detail-track.js
+++ js/detail-track.js
@@ -37,7 +37,42 @@ fetch(urlTrack)
 
     let recuperoStorage = localStorage.getItem("playlist");
 
+    if(recuperoStorage){ //null o undefined => false //tiene una variable => true
+        let playlistToArray = JSON.parse(recuperoStorage)
+        playlist = playlistToArray
+    }
 
+    let link= document.querySelector("a");
+
+    if(playlist.includes(idCancion)){
+        //cambiarle el mensaje al usuario.
+        link.innerText = "Sacar de playlist"
+    }
+
+    //Definir un evento para el link
+    link.addEventListener("click", function(evento){
+        //evitar default del link
+        evento.preventDefault()
+
+        if(playlist.includes(idGif)){
+            //Sacar el id del array
+            let gifASacar = playlist.indexOf(idCancion)
+            playlist.splice(gifASacar, 1);
+            link.innerText="Agregar a playlist"
+
+        } else {
+            //Agregar un data al array
+            playlist.push(idCancion);
+            link.innerText = "Sacar de playlist"
+
+        }
+    
+        //Agregar el array a localStorage. Antes hay que pasarlo a string
+        let gifFavoritosToString = JSON.stringify(playlist);
+        localStorage.setItem('playlist', gifFavoritosToString)
+
+
+        console.log(localStorage.getItem("playlist"));
 
 
 
@@ -47,4 +82,4 @@ fetch(urlTrack)
 .catch(function(error){
     console.log('el error fue ' + error);
 })
-
+})
